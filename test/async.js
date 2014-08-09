05e2edbf8ba97096e34adb64fad19d21d0c0dbcf
dvdrtrgn <dvdrtrgn@gmail.com>
2014-08-09 00:21:20 -0500

    reform self-doc

--- test/async.js
+++ test/async.js
@@ -1,71 +1,77 @@
 /*jslint es5:true, white:false */
 /*globals $, window */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-var B = $.now(),
-    W = (W || window),
+var W = (W || window),
     C = (W.C || W.console),
-    DFR, OBJ, RTN = {};
+    log = C.log.bind(C),
+    startTime = $.now();
 
-function random(max, min) {
-    min = min || 0;
-    return Math.floor(min + Math.random() * (max - min));
-}
-function chime() {
-    C.debug('Hey, guys it’s ' + ($.now() - B));
-}
-function log(x) {
-    C.log(x);
-}
-function hello(name) {
-    C.log('Hello ' + name);
-}
-function request(fn) {
-    var urgency = random(100, 1000);
-    W.setTimeout((fn || chime), urgency);
-    return urgency;
-}
-function status(dfr, sec) {
-    W.setTimeout(function working() {
-        if (dfr.state() === 'pending') {
-            dfr.notify($.now());
-            W.setTimeout(working, sec * 1000);
-        }
-    }, 0);
-}
+var U = (function (self) {
+    self = self || {};
+
+    $.extend(self, {
+        random: function (max, min) {
+            min = min || 0;
+            return Math.floor(min + Math.random() * (max - min));
+        },
+        chime: function () {
+            C.debug('Hey, guys it’s ' + ($.now() - startTime));
+        },
+        hello: function (name) {
+            C.log('Hello ' + name);
+        },
+        request: function (fn) {
+            var urgency = self.random(100, 1000);
+            W.setTimeout((fn || self.chime), urgency);
+            return urgency;
+        },
+        status: function (dfr, sec) {
+            W.setTimeout(function working() {
+                if (dfr.state() === 'pending') {
+                    dfr.notify($.now());
+                    W.setTimeout(working, sec * 1000);
+                }
+            }, 0);
+        },
+    });
+    return self;
+}());
 
 function t1() {
-    function asyncEvent() {
-        DFR = new $.Deferred();
+    var waiter, result = {};
+
+    function lazyOrder() {
+        waiter = new $.Deferred();
 
         // Resolve after a random interval
-        RTN.t1 = request(function () {
-            DFR.resolve('Timer 1 won');
+        result.t1 = U.request(function () {
+            waiter.resolve('Timer 1 won');
         });
         // Reject after a random interval
-        RTN.t2 = request(function () {
-            DFR.reject('Timer 1 lost');
+        result.t2 = U.request(function () {
+            waiter.reject('Timer 1 lost');
         });
 
         // Show a 'working...' message regularly
-        status(DFR, 0.1);
+        U.status(waiter, 0.1);
         // Return the Promise so caller can't change the Deferred
-        return DFR.promise();
+        return waiter.promise();
     }
-    // Attach [done, fail, progress] handlers for the asyncEvent
-    $.when(asyncEvent()).then(log, log, log);
-    return [DFR, RTN];
+    // Attach [done, fail, progress] handlers for the lazyOrder
+    $.when(lazyOrder()).then(log, log, log);
+    return [waiter, result];
 }
 
 function t2() {
-    DFR = $.Deferred();
-    OBJ = { // becomes a promise piggybacked upon an async scenerio
-        hello: hello,
+    var waiter = $.Deferred();
+    var thingy = { // becomes a promise piggybacked upon an async scenerio
+        hello: U.hello,
     };
 
-    DFR.promise(OBJ); //    extend OBJ  promise api
-    DFR.resolve('Jon'); //  mutate DFR  locked data
-    OBJ.done(function (x) {
-        OBJ.hello(x); //    Hello Jon
-    }).hello('Karl');  //   invoke promise
-    return [DFR, OBJ, RTN];
+    waiter.promise(thingy); //  extend thingy  promise api
+    waiter.resolve('Jon'); //   mutate waiter  locked data
+    thingy.done(function (x) {
+        thingy.hello(x); //     Hello Jon
+    }).hello('Karl');  //       invoke promise
+    return [waiter, thingy];
 }
